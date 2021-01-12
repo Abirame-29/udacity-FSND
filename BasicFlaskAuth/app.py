c8d8044dcd25ca9bf05ef85885b1663550cbcbbd
Abirame-29 <nalinidhevan@gmail.com>
2021-01-13 00:46:40 +0530

    Folder structure updated

---
+++ BasicFlaskAuth/app.py
@@ -0,0 +1,142 @@
+from flask import Flask, request, abort, jsonify
+import json
+from functools import wraps
+from jose import jwt
+from urllib.request import urlopen
+
+
+app = Flask(__name__)
+
+AUTH0_DOMAIN = 'abirame.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'image'
+
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+def check_permissions(permissions, payload):
+    if 'permissions' not in payload:
+        abort(400)
+    if permissions not in payload['permissions']:
+        abort(403)
+    return True
+
+def get_token_auth_header():
+    """Obtains the Access Token from the Authorization Header
+    """
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+        print(request.headers)
+        raise AuthError({
+            'code': 'authorization_header_missing',
+            'description': 'Authorization header is expected.'
+        }, 401)
+
+    parts = auth.split()
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Token not found.'
+        }, 401)
+
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization header must be bearer token.'
+        }, 401)
+
+    token = parts[1]
+    return token
+
+
+def verify_decode_jwt(token):
+    jsonurl = urlopen(f'https://abirame.us.auth0.com/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    if 'kid' not in unverified_header:
+        raise AuthError({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+
+    for key in jwks['keys']:
+        if key['kid'] == unverified_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://abirame.us.auth0.com/'
+            )
+
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+            }, 400)
+
+
+def requires_auth(permissions=''):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(token)
+            except:
+                abort(401)
+            check_permissions(permissions, payload)
+            return f(payload, *args, **kwargs)
+        return wrapper
+    return requires_auth_decorator
+
+@app.route('/images')
+@requires_auth('get:images')
+def images(payload):
+    print(payload)
+    return 'Access Granted'
+
+@app.errorhandler(AuthError)
+def unauthorized(error):
+    return jsonify({
+            'success': False,
+            'error': 500,
+            'message': 'Internal server error'
+        }), 401
